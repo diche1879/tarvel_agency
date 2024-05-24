918d5bbad99e84c013759ab1df3e27d26f9eb1da
diche1879 <147421870+diche1879@users.noreply.github.com>
2024-05-24 10:46:15 +0200

    versión final

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
+// Importaciones necesarias
 const express = require('express');
+// Inicialización de Express
 const app = express();
 
+// creo una variable con los datos del Json
 const travels = require('./data/travels.json');
+
+// Puerto de escucha
 const PORT = 3000;
 
+// Configuración de Express y su motor de plantillas ejs
 app.set('view engine', 'ejs');
+// ruta para la carpeta donde se encuentran los ficheros ejs
 app.set('views', __dirname + '/views');
-
+// uso y ruta para/de la carpeta de los ficheros estáticos
 app.use(express.static(__dirname + '/public'));
 
-// Función unica para cada página de viaje
-// se pasan tres parametro donde el travelId es el id del viaje que se quiere mostrar
-// Se toma desde la url y sería la pagina de direccion que se considera como un parametro
-const travelPage = (req, res, travelId) => {
-    const travel = travels.find((travel) => travel.id === travelId);
-    if (travel) {
+travels.forEach((travel) => {
+    app.get(`/${travel.id}`, (req, res) => {
         res.render('index', {
+            travels: travels,
             id: travel.id,
             lugar: travel.lugar,
             nombre: travel.nombre,
@@ -23,116 +27,12 @@ const travelPage = (req, res, travelId) => {
             precio: travel.precio,
             img: travel.img
         });
-    } else {
-        res.status(404).send('Elemento de viaje no encontrado');
-    }
-};
-
-// Ruta para la página principal
-app.get('/', (req, res) => {
-    travelPage(req, res, "");
+    });
 });
 
-// Ruta dinámica para las páginas de destinos
-//recupera el parametro de la url y lo pasa a la función travelPage
-app.get('/:destinacion', (req, res) => {
-    const destinacion = req.params.destinacion;
-    travelPage(req, res, destinacion);
+app.use((req, res) => {
+    res.status(404).render('error404')
 });
-
-/* app.get('/', (req, res) => {
-    const travel = travels.find((travel) => travel.id == "");
-    if (travel) {
-        res.render('index', {
-            id: travel.id,
-            lugar: travel.lugar,
-            nombre: travel.nombre,
-            descripcion: travel.descripcion,
-            precio: travel.precio,
-            img: travel.img
-        });
-    } else {
-        res.status(404).send('Elemento de viaje no encontrado');
-    }
-});
-app.get('/maldivas', (req, res) => {
-    const travel = travels.find((travel) => travel.id == "maldivas");
-    if (travel) {
-        res.render('index', {
-            id: travel.id,
-            lugar: travel.lugar,
-            nombre: travel.nombre,
-            descripcion: travel.descripcion,
-            precio: travel.precio,
-            img: travel.img
-        });
-    } else {
-        res.status(404).send('Elemento de viaje no encontrado');
-    }
-});
-
-app.get('/egipto', (req, res) => {
-    const travel = travels.find((travel) => travel.id == "egipto");
-    if (travel) {
-        res.render('index', {
-            id: travel.id,
-            lugar: travel.lugar,
-            nombre: travel.nombre,
-            descripcion: travel.descripcion,
-            precio: travel.precio,
-            img: travel.img
-        });
-    } else {
-        res.status(404).send('Elemento de viaje no encontrado');
-    }
-});
-app.get('/nuevayork', (req, res) => {
-    const travel = travels.find((travel) => travel.id == "nuevayork");
-    if (travel) {
-        res.render('index', {
-            id: travel.id,
-            lugar: travel.lugar,
-            nombre: travel.nombre,
-            descripcion: travel.descripcion,
-            precio: travel.precio,
-            img: travel.img
-        });
-    } else {
-        res.status(404).send('Elemento de viaje no encontrado');
-    }
-});
-app.get('/venecia', (req, res) => {
-    const travel = travels.find((travel) => travel.id == "venecia");
-    if (travel) {
-        res.render('index', {
-            id: travel.id,
-            lugar: travel.lugar,
-            nombre: travel.nombre,
-            descripcion: travel.descripcion,
-            precio: travel.precio,
-            img: travel.img
-        });
-    } else {
-        res.status(404).send('Elemento de viaje no encontrado');
-    }
-});
-
-app.get('/vietnam', (req, res) => {
-    const travel = travels.find((travel) => travel.id == "vietnam");
-    if (travel) {
-        res.render('index', {
-            id: travel.id,
-            lugar: travel.lugar,
-            nombre: travel.nombre,
-            descripcion: travel.descripcion,
-            precio: travel.precio,
-            img: travel.img
-        });
-    } else {
-        res.status(404).send('Elemento de viaje no encontrado');
-    }
-}); */
-
 
 app.listen(PORT, () => {
     console.log(`El servidor está escuchando en el puerto http://localhost:${PORT}`);
